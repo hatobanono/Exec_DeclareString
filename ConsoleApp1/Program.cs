229acf492fcc610ed28d7bab5624ecdc6f2d3526
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 09:29:49 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("c:\\temp\\a.jpg");
+			Console.WriteLine("我是一個\"好學生\"");
+			Console.WriteLine("我是第一行\n我是第二行");
+		}
+	}
+}
